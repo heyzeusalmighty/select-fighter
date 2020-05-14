5ba0568c79371ba20336e8b53d76a555d69f0d78
Thomas Lombardi <thomas.lombardi@wholefoods.com>
2020-05-14 08:37:27 -0500

    trying something

--- src/components/big-hero/index.js
+++ src/components/big-hero/index.js
@@ -1,23 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './style.css';
 
+
+const fetchHeroQuote = name => {
+  return 'One of a race of persons who lived before the division of labor had been carried to such a pitch of differentiation, and who followed the primitive economic maxim, “Every man his own horse.” The best of the lot was Chiron, who to the wisdom and virtues of the horse added the fleetness of man.';
+}
+
+
+
+
 const BigHero = ({ hero }) => {
+  const [quote, setQuote] = useState('');
+
   useEffect(() => {
     console.log('new hero', hero.name)
+    setQuote(fetchHeroQuote(hero.fakeName))
   }, [hero]);
 
   return (
     <div className='big-hero-container'>
-      <div>I MAM HERO - {hero.name}</div>
+      <div className='hero-name'>{hero.fakeName}</div>
       <div className="info-panel">
         <div className='hero-big-picture-container'>
           <img src={hero.largeImage} alt='omg' className='big-hero-picture' />
         </div>
         <div className='hero-big-info-container'>
-          stuff
-          stuff
-          stuff
+          {quote}
         </div>
       </div>
       
--- src/pages/select.js
+++ src/pages/select.js
@@ -1,15 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BigHero from '../components/big-hero';
 import Hero from '../components/hero/index';
 
 import fighters from '../data/fighters';
 import './select.css';
 
+const shuffle = array => {
+  let currentIdx = array.length, temporaryValue, randomIdx;
+  while (0 !== currentIdx) {
+    randomIdx = Math.floor(Math.random() * currentIdx);
+    currentIdx -= 1;
+
+    temporaryValue = array[currentIdx];
+    array[currentIdx] = array[randomIdx];
+    array[randomIdx] = temporaryValue;
+  }
+  return array;
+}
+
+
+const dummyUsers = {
+  v: {},
+  eric: {},
+  tyler: {},
+  scot: {},
+  dustin: {},
+  steven: {},
+  aaron: {},
+  thomas: {},
+  jaime: {},
+  james: {},
+}
+
+
 const SelectScreen = () => {
   const [selectedHero, setSelectedHero] = useState(fighters[0]);
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(() => {
+    const shuffled = shuffle(Object.keys(dummyUsers));
+    const newFighters = fighters.map((x, idx) => ({
+      ...x,
+      fakeName: shuffled[idx],
+    }));
+    setHeroes(newFighters);
+    setSelectedHero(newFighters[0]);
+  }, []);
 
   const onSelection = id => {
-    const hero = fighters.find(x => x.id === id);
+    const hero = heroes.find(x => x.id === id);
     if (hero) {
       setSelectedHero(hero);
     }
@@ -22,7 +61,7 @@ const SelectScreen = () => {
         <BigHero hero={selectedHero} />
       </div>
       <div className="hero-selection-grid">
-        {fighters.map(x => (
+        {heroes.map(x => (
           <Hero
             hero={x}
             isSelected={x.id === selectedHero.id}
